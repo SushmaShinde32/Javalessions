bbd5d9182641d767ede13e2ac13062c4b3cf1d63
Sushma Shinde <sushshinde32@gmail.com>
2024-02-12 13:39:01 +0200

    Added code for arithmatic operations

---
+++ src/BasicMathOperations.java
@@ -0,0 +1,31 @@
+public java.io.*;
+
+public class BasicMathOperations {
+
+    public static void main(String[] args) {
+        // Declare variables
+        int num1 = 10;
+        int num2 = 5;
+
+        // Addition
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int difference = num1 - num2;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication
+        int product = num1 * num2;
+        System.out.println("Product: " + product);
+
+        // Division
+        // Note: Division by zero should be handled separately to avoid ArithmeticException
+        if (num2 != 0) {
+            double quotient = (double) num1 / num2;
+            System.out.println("Quotient: " + quotient);
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+    }
+}
